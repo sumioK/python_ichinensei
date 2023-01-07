66b6aa626215eff3937403fe59063f43355d1e55
Sumio K <tap.miwo@gmail.com>
2023-01-07 14:56:17 +0900

    import

---
+++ import.py
@@ -0,0 +1,5 @@
+from mod.tax import postTaxPrice
+
+print(postTaxPrice(120),'円')
+print(postTaxPrice(500),'円')
+print(postTaxPrice(1000),'円')
---
+++ mod/tax.py
@@ -0,0 +1,4 @@
+def postTaxPrice(price):
+  ans = price * 1.1
+  return ans
+  
