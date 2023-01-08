79d5d360ba91c1bb2a9dbbadc4c27f7e779dc7da
Sumio K <tap.miwo@gmail.com>
2023-01-08 20:58:57 +0900

    learning app

---
+++ AI/predictDigits.py
@@ -0,0 +1,27 @@
+import sklearn.datasets
+import sklearn.svm
+import PIL.Image
+import numpy
+
+def imageToData(filename):
+  grayImage = PIL.Image.open(filename).convert('L')
+  grayImage = grayImage.resize((8, 8),PIL.Image.Resampling.LANCZOS)
+
+  numImage = numpy.asarray(grayImage, dtype = float)
+  numImage = 16 - numpy.floor(17 * numImage /256)
+  numImage = numImage.flatten()
+
+  return numImage
+
+def predicDigits(data):
+  digits = sklearn.datasets.load_digits()
+
+  clf = sklearn.svm.SVC(gamma=0.001)
+  clf.fit(digits.data, digits.target)
+
+  n = clf.predict([data])
+  print("予測＝", n)
+
+data = imageToData("2.png")
+
+predicDigits(data)
